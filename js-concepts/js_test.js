48370b9e31bfe5140b23d32f65b6ac7296cc34a2
abhisaharan <abhi.saharan@gmail.com>
2018-09-22 19:03:55 -0700

    more notes included

--- js-concepts/js_test.js
+++ js-concepts/js_test.js
@@ -1,8 +1,3 @@
-a = [2,4,6,7];
-var i;
-for (i=0; i<= a.length; i++) {
-    if (a[i] === 4) {
-        a.splice(i,i);
-    }
-}
-console.log(a);
+var d = Date.parse("2015-03-25T12:00:00-06:00");
+
+console.log(d);
